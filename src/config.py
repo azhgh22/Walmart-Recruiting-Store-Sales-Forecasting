75dc2c1a68899f27d217e4ae93d2c63a8071e4c8
Mateus929 <61325817+Mateus929@users.noreply.github.com>
2025-06-29 19:00:08 +0400

    Add SPLIT_DATE to config.py

--- src/config.py
+++ src/config.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import pandas as pd
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -16,6 +17,7 @@ SAMPLE_SUBMISSION_PATH = DATA_DIR / "sampleSubmission.csv"
 # time-series validation strategy later in a project.
 TRAIN_RATIO = 0.8 
 VAL_RATIO = 0.2
+SPLIT_DATE = pd.Timestamp('2011-11-30') 
 
 TARGET_COLUMN = "Weekly_Sales"
 DATE_COLUMN = "Date"
