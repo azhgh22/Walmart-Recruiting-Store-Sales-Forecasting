c5dc1e2ff87171d3138efc6ec6af32bbb3523769
Mateus929 <zhorzholianimate@gmail.com>
2025-07-04 22:55:51 +0000

    add sarima configs

--- configs/linear_model_configs.py
+++ configs/linear_model_configs.py
@@ -4,3 +4,23 @@ arima_config = {
     'filterwarnings': True,
     'verbose': 0
 }
+
+sarima_config = {
+    "model_name": "StoreDeptSARIMAX",
+    "order": (1, 1, 1),
+    "seasonal": True,
+    "seasonal_order": (1, 1, 1, 52), 
+    "use_all_exog": False,
+    "filterwarnings": True,
+    "verbose": 0
+}
+
+sarimax_config = {
+    "model_name": "StoreDeptSARIMAX",
+    "order": (1, 1, 1),
+    "seasonal": True,
+    "seasonal_order": (1, 1, 1, 52), 
+    "use_all_exog": False,
+    "filterwarnings": True,
+    "verbose": 0
+}
