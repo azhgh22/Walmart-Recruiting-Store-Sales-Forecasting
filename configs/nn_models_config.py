da0514d1b6fc025ff2052a9cfd044f7fcdf84536
Mateus929 <zhorzholianimate@gmail.com>
2025-07-07 13:11:53 +0000

    add nn configs

---
+++ configs/nn_models_config.py
@@ -0,0 +1,9 @@
+patchtst_config = {
+    'input_size': 52,           
+    'h': 53,                  
+    'max_steps': 60 * 104,    
+    'batch_size': 64,
+    'random_seed': 42,
+    'activation': 'relu',     
+    'dropout': 0.2,           
+}
