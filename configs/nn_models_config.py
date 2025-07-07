f33f0b0f8ff976245e5fb44a6185650f39f5c6b9
Archil <145471762+azhgh22@users.noreply.github.com>
2025-07-07 21:56:54 +0400

    Update nn_models_config.py, added nbeats__config

--- configs/nn_models_config.py
+++ configs/nn_models_config.py
@@ -7,3 +7,17 @@ patchtst_config = {
     'activation': 'relu',     
     'dropout': 0.2,           
 }
+
+
+nbeats_config = {
+    'max_steps': 25 * 104,
+    'h': 53,
+    'random_seed': 42,
+    'input_size':52,
+    'batch_size': 256,
+    'learning_rate': 1e-3,
+    'shared_weights':True,
+    'optimizer': 'torch.optim.AdamW',
+    'activation': 'ReLU'
+
+}
